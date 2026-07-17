135ce684e01c1458755b110e84f3af5626928958
Angel Aspuru <angel.aspuru@unosquare.com>
2026-07-17 15:53:28 -0600

    folder fiesta

--- Front-End Deliverable/data/product-data.ts
+++
@@ -1,8 +0,0 @@
-export const products = {
-    backpack: 'backpack',
-    bike: 'bike-light',
-    t_shirt: 't-shirt',
-    jacket: 'jacket',
-    onesie: 'onesie',
-    t_shirt_red: 't-shirt-(red)',
-};
